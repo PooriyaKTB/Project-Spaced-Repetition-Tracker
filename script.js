8c075209088cb647eedfcfe5eb3bb15003175502
Pooriya Ketabi <pooriya.ketabi@gmail.com>
2025-02-03 18:38:39 +0000

    Implement JS file with DOM elements

---
+++ script.js
@@ -0,0 +1,8 @@
+// DOM Elements
+const userSelect = document.getElementById('user-select');
+const agendaList = document.getElementById('agenda-list');
+const noAgendaMessage = document.getElementById('no-agenda-message');
+const addTopicForm = document.getElementById('add-topic-form');
+const topicNameInput = document.getElementById('topic-name');
+const topicDateInput = document.getElementById('topic-date');
+const clearDataButton = document.getElementById('clear-data-button');
